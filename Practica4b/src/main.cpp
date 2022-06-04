8b5d409f39260bc1aeb0eab25fd068d5dd7855c7
AlbertBatllo <100155905+AlbertBatllo@users.noreply.github.com>
2022-06-04 13:29:30 +0200

    Update main.cpp

--- Practica4b/src/main.cpp
+++ Practica4b/src/main.cpp
@@ -1,9 +1,36 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
+void LED_ON (void * pvParameters);
+void LED_OFF(void * pvParameters);
+
+int LED =2; 
+
+SemaphoreHandle_t semaphore;
+
+void setup(){
+
+Serial.begin(115200);
+pinMode(LED, OUTPUT);
+semaphore= xSemaphoreCreateMutex();
+xTaskCreate(LED_ON, "ON", 10000, NULL, 1, NULL);
+xTaskCreate(LED_OFF, "OFF", 10000, NULL, 1, NULL);
+
+void loop(){}
+
+void LED_ON (void * pvParameters){
+    for(;;){
+        xSemaphoreTake(semaphore, portMAX_DELAY);
+        digitalWrite(LED, HIGH);
+        delay(3000);
+        xSemaphoreGive(semaphore);
+    }
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void LED_OFF (void * pvParameters){
+    for(;;){
+        xSemaphoreTake(semaphore, portMAX_DELAY);
+        digitalWrite(LED, LOW);
+        delay(1500);
+        xSemaphoreGive(semaphore);
+    }
 }
